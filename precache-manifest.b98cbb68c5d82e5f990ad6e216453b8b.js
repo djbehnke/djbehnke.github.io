53b15f70aa36ce169745a2104a3519e8bb2458ab
djbehnke <djbehnke.mail@gmail.com>
2020-01-31 23:47:34 -0500

    Upgrade Vue Version and wipe.

---
+++ precache-manifest.b98cbb68c5d82e5f990ad6e216453b8b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e4bfe18cf7e85077fb0",
+    "url": "/css/app.31396d03.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "00428fae7d80aa91208190109ad01c54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5813ea769c7e6b5bd4bb",
+    "url": "/js/about.2b389b99.js"
+  },
+  {
+    "revision": "8e4bfe18cf7e85077fb0",
+    "url": "/js/app.ade614f5.js"
+  },
+  {
+    "revision": "98f22f64d7e45c8b32fa",
+    "url": "/js/chunk-vendors.ec909805.js"
+  },
+  {
+    "revision": "c0ada6d4be4bb20b9b691cb8601a9951",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
